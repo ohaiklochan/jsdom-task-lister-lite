3eaf96da89e8e55900d1c4a7c93ff10da8013f23
ohaiklochan <caforonda103@yahoo.com>
2021-03-08 23:10:34 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.getElementById('create-task-form')
+  const taskList = document.getElementById('tasks')
+  const taskName = document.getElementById('new-task-description')
+
+  function buildTaskList() {
+    let task = document.createElement("li")
+    task.textContent = taskName.value + ' '
+    let button = document.createElement("button")
+    button.textContent = 'x'
+    task.appendChild(button)
+
+    return task
+  }
+
+  function addOtherTasks() {
+    event.preventDefault()
+    task = buildTaskList()
+    list.appendChild(task)
+    event.target.reset()
+  }
+  form.addEventListener("submit", addOtherTasks)
 });
